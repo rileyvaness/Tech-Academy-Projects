3a618e509796aa99a1b71f2d72d838ca39d5f5a4
Riley Van Ess <rileyvaness@gmail.com>
2018-07-31 00:12:45 -0400

    AcmeAccounting Message

---
+++ Demo/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// A Hello World! program in C#.
+using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.WriteLine();
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
