018e05707c099d698ad3897ee4a11c29593868e0
Riley Van Ess <rileyvaness@gmail.com>
2018-08-08 09:52:17 -0400

    Class and Methods

---
+++ ClassAndMethods/ClassAndMethods/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Please provide a number to use in some calculations by this program: ");
+            try
+            {
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+                //passing the userNumber through provided information 
+                //from the math class and printing it to the screen. 
+                Console.WriteLine(Math.Addition(userNumber));
+                Console.WriteLine(Math.Division(userNumber));
+                Console.WriteLine(Math.Multipication(userNumber));
+            }
+            //error message in case some one does not write a number.
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ClassAndMethods/ClassAndMethods/math.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods
+{
+    class Math
+        //when Math is called in the program it consults this class.
+    {
+        public static int Addition(int userNumber)
+        {
+            return userNumber + 72;
+        }
+        public static int Division(int userNumber)
+        {
+            return userNumber / 2;
+        }
+        public static int Multipication(int userNumber)
+        {
+            return userNumber * 22;
+        }
+    }
+}
