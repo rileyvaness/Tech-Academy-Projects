bc4c445a1e799ffda9bd009bc6cfc369de9a725f
Riley Van Ess <rileyvaness@gmail.com>
2018-08-08 13:11:35 -0400

    Class and Methods, part 3

---
+++ ClassAndMethods, part3/ClassAndMethods, part3/Math.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods__part3
+{
+    class Math
+    {
+        public static int Addition (int number)
+
+         {
+
+            int newNumber = number + 9;
+            return newNumber;
+         }
+
+
+
+        public static decimal NewAddition(decimal number2)
+
+
+        {
+
+            decimal newNumber = number2 + 3;
+            return newNumber;
+
+        }
+
+
+
+        public static int String(string myString)
+
+        {
+
+            bool toInt = int.TryParse(myString, out int convertedString);
+            int newConvertedString = convertedString + 25;
+            return newConvertedString;
+
+        }
+
+    }
+
+}
---
+++ ClassAndMethods, part3/ClassAndMethods, part3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods__part3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Here I pass in an integer for some basic addition
+            //to be referenced from the math clas.
+            Math operation = new Math();
+            int number= 1;
+            int result = Math.Addition(number);
+            Console.WriteLine("The answer is: {0}", result);
+            Console.ReadLine();
+        
+
+            //Here I pass in a decimal
+            //to be refreneced from the math class.
+            decimal number2 = 94.23m;
+            decimal result2 = Math.NewAddition(number2);
+            Console.WriteLine("The new answer is: {0}", result2);
+            Console.ReadLine();
+
+
+            //Here is a number, disguised as a string. 
+            //In the math class, I convert the string to an Integer, 
+            // thus allowing me to use preform the math I call for. 
+            string number3 = "22";
+            int result3 =  Math.String(number3);
+            Console.WriteLine("The newest answer is: {0}", result3);
+             Console.ReadLine();
+
+
+
+        }
+    }
+}
