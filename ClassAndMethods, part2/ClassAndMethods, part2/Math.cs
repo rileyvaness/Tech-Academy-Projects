d4cbb47b90805f0cd1c2a4d977169448f2a41433
Riley Van Ess <rileyvaness@gmail.com>
2018-08-08 11:51:14 -0400

    Class and Methods, part 2

---
+++ ClassAndMethods, part2/ClassAndMethods, part2/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods__part2
+{
+    class Math
+    {
+        public static int Add(int number)
+        {
+            int number1 = number + 9; 
+            return number1;
+        }
+    }
+}
---
+++ ClassAndMethods, part2/ClassAndMethods, part2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods__part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Math Addition = new Math();
+            int number = 1;
+            int result = Math.Add(number);
+            Console.WriteLine("Answer = {0}. " , result);
+            Console.ReadLine();
+        }
+    }
+}
