201ddd72402ad9e08a92ab3959b98fb77e8faac2
Riley Van Ess <rileyvaness@gmail.com>
2018-08-14 11:24:16 -0400

    Constructors and Const Variables

---
+++ ConstructorsAndConstVariables/ConstructorsAndConstVariables/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsAndConstVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //#1: Create a const variable
+            const string shakespeareShow = "Antony and Cleopatra";
+            Console.WriteLine("We are currently performing, {0}, this weekend. ", shakespeareShow);
+            
+            
+            //#2 Create a variable using "var"
+            var TheaterTroupe = "Prague Shakespeare Company";
+            Console.WriteLine("The show will be performed by the {0}.", TheaterTroupe);
+            //#3 Chain two constructors together
+
+            Tickets tickets = new Tickets();
+            {
+                Console.WriteLine("There are still " + tickets.Show + " tickets left! Get your tickets now!");
+               
+            }
+
+            Console.ReadLine();
+
+        }
+
+
+
+
+    }
+}
---
+++ ConstructorsAndConstVariables/ConstructorsAndConstVariables/Tickets.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsAndConstVariables
+{
+    class Tickets
+    {
+        int show;
+        public Tickets() => show = 41;
+        public int Show { get => show; }
+
+    }
+}
