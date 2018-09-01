befb0e1af60af5bfcdee620a810916e141ba62a3
Riley Van Ess <rileyvaness@gmail.com>
2018-08-31 20:29:42 -0400

    MVC 5, Security and creating a user role

---
+++ MVCUSERROLE/MVCUSERROLE/Controllers/UsersController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security.Claims;
+using System.Web.Security;
+using Microsoft.AspNet.Identity;
+using MVCUSERROLE.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MVCUSERROLE.Controllers
+{
+    [Authorize]
+    public class UsersController : Controller
+    {
+        public Boolean isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ApplicationDbContext context = new ApplicationDbContext();
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+        // GET: usercontroller
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ViewBag.Name = user.Name;
+
+                ViewBag.displayMenu = "No";
+
+                if (isAdminUser())
+                {
+                    ViewBag.displayMenu = "Yes";
+                }
+                return View();
+            }
+            else
+            {
+                ViewBag.Name = "Not Logged IN";
+            }
+            return View();
+
+        }
+       
+    }
+}
